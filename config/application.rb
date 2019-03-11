73592ca4640327e12644721303965de7a48eb4fa
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-11 20:18:02 +0000

    New fonts

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Nomster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    config.assets.precompile += %w(.svg eot. .woff .ttf)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
