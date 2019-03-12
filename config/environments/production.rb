183d85fac303ade19b25b36e6fa4f1efe2d90d8c
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-12 22:22:51 +0000

    Added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  config.action_mailer.default_url_options = { host: 'nomster-wilson-horrell.herokuapp.com' }
+
+
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -92,3 +93,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
