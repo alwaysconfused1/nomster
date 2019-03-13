c81c1863f9803f227c2ab156cd7977784161e29d
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-13 22:53:53 +0000

    Added Google API for geolocating

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@ ruby '2.5.3'
 gem 'jquery-rails'
 
 gem 'kaminari'
+
+gem 'geocoder'
+
+gem 'figaro'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use postgresql as the database for Active Record
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
 	belongs_to :user
-	validates :name, presence: true
+	geocoded_by :address
+	after_validation :geocode
+	validates :name, presence: true, length: {minimum: 3, too_short: "Name must be greater than %{count} characters."}
+	validates :address, :description, presence: true
+
 end
