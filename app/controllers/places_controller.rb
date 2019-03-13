26b57dbda39e9e5983fe392569491282e4d07cc0
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-13 18:13:03 +0000

    Removed option to edit place if not logged in as user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]
-  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
 
   def index
     @places = Place.order("name").page(params[:page]).per(3)
@@ -15,18 +15,27 @@ before_action :authenticate_user!, only: [:new, :create]
     redirect_to root_path
   end
 
-  def show 
+  def show
     @place = Place.find(params[:id])
   end
 
-def edit
-  @place = Place.find(params[:id])
-end
+  def edit
+    @place = Place.find(params[:id])
 
-def update
-  @place = Place.find(params[:id])
-  @place.update_attributes(place_params)
-  redirect_to root_path
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+
+    end
+
+    @place.update_attributes(place_params)
+    redirect_to root_path
   end
 
   def destroy
