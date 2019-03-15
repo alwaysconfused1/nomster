02488664190916f510d9ab5c4a05f13afc638575
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-15 17:44:50 +0000

    Added database for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
 	belongs_to :user
+	has_many :comments
+
 	geocoded_by :address
 	after_validation :geocode
+	
 	validates :name, presence: true, length: {minimum: 3, too_short: "Name must be greater than %{count} characters."}
 	validates :address, :description, presence: true
 
