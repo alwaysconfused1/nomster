3be7244a53a1bd40d63e72d6ee816ca1c36c9eca
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-16 13:23:49 +0000

    Attempting to add photos to app

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,11 @@
 class Place < ApplicationRecord
 	belongs_to :user
 	has_many :comments
+	has_many :photos
 
 	geocoded_by :address
 	after_validation :geocode
-	
+
 	validates :name, presence: true, length: {minimum: 3, too_short: "Name must be greater than %{count} characters."}
 	validates :address, :description, presence: true
 
