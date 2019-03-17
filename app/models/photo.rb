89bf5d1a0b77104c1a96eb9100b997f03bf5e106
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-17 12:30:32 +0000

    Still having problems with image_tag photo.picture.url. Says Nil location provided. Can't build URI

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,5 +4,7 @@ class Photo < ApplicationRecord
 	belongs_to :place
 
 	mount_uploader :picture, PictureUploader
+	mount_uploader :thumb_image, PictureUploader
+	mount_uploader :main_image, PictureUploader
 	
 end
