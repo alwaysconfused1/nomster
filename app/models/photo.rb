bacad6a1c04f460bf87fdaabaf4cc0d47ee3e84c
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-17 18:48:21 +0000

    <%= image_tag photo.picture.url %> Nil location provided. Can't build URI.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,6 @@ class Photo < ApplicationRecord
 	belongs_to :place
 
 	mount_uploader :picture, PictureUploader
-	mount_uploader :thumb_image, PictureUploader
-	mount_uploader :main_image, PictureUploader
+	
 	
 end
--- db/migrate/20190316155138_add_picture_photos.rb
+++ db/migrate/20190316155138_add_picture_photos.rb
@@ -1,4 +1,6 @@
 class AddPicturePhotos < ActiveRecord::Migration[5.2]
   def change
+
+  	add_column :photos, :picture, :string
   end
 end
