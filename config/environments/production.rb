1843c6bc45bfc572f3f8f14757a8fd421994ef2e
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-22 21:47:47 +0000

    Changed my uploader.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ ActionMailer::Base.smtp_settings = {
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
